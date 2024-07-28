58086f43b503b8cab76856b729d2f5d8a046a22b
Sumit Dhakal <dhakalsumeet123@gmail.com>
2024-07-28 21:19:09 +0545

    animation

--- lib/implicit_examples/animated_shopping_cart_button.dart
+++ lib/implicit_examples/animated_shopping_cart_button.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
 
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool isExpanded = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,16 +16,33 @@ class ShoppingCartButton extends StatelessWidget {
         title: const Text('Shopping Cart'),
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            width: isExpanded ? 150 : 80,
+            height: isExpanded ? 40 : 60.0,
+            decoration: BoxDecoration(
+              color: isExpanded ? Colors.green : Colors.blue,
+              borderRadius: isExpanded
+                  ? BorderRadius.circular(30.0)
+                  : BorderRadius.circular(10.0),
+            ),
+            child: isExpanded
+                ? Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [Icon(Icons.check), Text("Added to cart")],
+                    ),
+                  )
+                : const Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
           ),
         ),
       ),
